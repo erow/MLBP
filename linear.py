9962dfefcfeee6221483e723b032e118826e0479
erow <clouderow@gmail.com>
2019-01-16 22:34:32 +0800

    修复梯度下降计算的错误。

--- linear.py
+++ linear.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # init data
 n = 1000
-k = 2  # Todo not works
+k = 3  # fixed
 X = np.random.rand(n, k)*10   # x belongs to [0,10]
 b = 0.1
 theta = np.random.rand(1,k)
@@ -10,21 +10,21 @@ Y = np.dot(X, np.transpose(theta)) + b
 
 
 # init parameters
-tt = np.random.rand(1,k)
+tt = np.random.rand(k,1)
 theta0 = 0
 
 # gradient decrease
-a = 0.0005*16
+a = 0.01
 loss = 0
 it = 0
-while it<30000:
-    diff = np.dot(X, np.transpose(tt)) + theta0 - Y
-    tt -= a * np.sum(diff * X )/n
-    theta0 -= a * np.sum(diff)/n
+while it < 3500:
+    diff = np.dot(X, tt) + theta0 - Y  # nx1
+    tt -= a/n * np.dot(np.transpose(X), diff)  # kx1
+    theta0 -= a/n * np.sum(diff)
     loss1 = np.sum(np.square(diff))/2/n
     print("theta0:{},d:{}, loss:{}".format(theta0,a * np.sum(diff)/n, loss1))
     loss = loss1
     it += 1
 
 print("real:{},{}".format(theta,b))
-print("parameters:{},{}".format(tt,theta0))
+print("parameters:{},{}".format(np.transpose(tt),theta0))
